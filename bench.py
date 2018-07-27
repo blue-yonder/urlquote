92f1fd3138b8422527fe347aebee4b1d7d818759
Markus Klein <markus.klein@blue-yonder.com>
2018-07-27 11:13:07 +0200

    refactor benchmarks

--- bench.py
+++ bench.py
@@ -2,28 +2,32 @@
 '''I (mklein) have no idea how to write benchmarks in python. These benchmarks are highly
    doubtful!!!'''
 
-from urlquote import quote, unquote
+LOREMIPSUM = """
+[32] Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet, consectetur, adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
+[33] At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat...
+""".encode('utf-8')
+
+import urlquote
+from urllib.parse import quote as urllib_quote
 import timeit
 
+def benchmark_quote_urlquote():
+    urlquote.quote(LOREMIPSUM)
+
+def benchmark_quote_urllib():
+    urllib_quote(LOREMIPSUM)
+
 if __name__ == "__main__":
-    print("Quote")
-    setup = "from urlquote import quote, unquote\ninput=u'/El Niño/'.encode('utf-8')"
-    expression = "quote(input)"
+    print("This library")
+    setup = "from __main__ import benchmark_quote_urlquote"
+    expression = "benchmark_quote_urlquote()"
     repetitions = 100000
     duration = timeit.timeit(expression, setup=setup, number = repetitions)
-    print("Non trivial case:")
-    print("Total duration: ", duration, "Average Duration: ", duration / repetitions)
-    expression = "quote(input)"
-    duration = timeit.timeit(expression, setup=setup, number = repetitions)
-    print("Trivial case:")
     print("Total duration: ", duration, "Average Duration: ", duration / repetitions)
 
-    # print("Unquote")
-    # expression = "unquote('/El%20Ni%C3%B1o/')"
-    # duration = timeit.timeit(expression, setup=setup, number = repetitions)
-    # print("Non trivial case:")
-    # print("Total duration: ", duration, "Average Duration: ", duration / repetitions)
-    # expression = "unquote('/El-Nino/')"
-    # duration = timeit.timeit(expression, setup=setup, number = repetitions)
-    # print("Trivial case:")
-    # print("Total duration: ", duration, "Average Duration: ", duration / repetitions)
+    print("Urllib")
+    setup = "from __main__ import benchmark_quote_urllib"
+    expression = "benchmark_quote_urllib()"
+    repetitions = 100000
+    duration = timeit.timeit(expression, setup=setup, number = repetitions)
+    print("Total duration: ", duration, "Average Duration: ", duration / repetitions)
