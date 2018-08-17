3dce86cf8fd57ecf3bdd41021c939cdfc74678d5
Matthias Bach <matthias.bach@blue-yonder.com>
2018-08-17 11:14:12 +0200

    Fix assertion syntax in tests
    Assert in Python is a statement not a function.

--- test.py
+++ test.py
@@ -4,19 +4,19 @@ from urlquote import quote, unquote
 def test_quote_bytes():
     expected = u'/El%20Ni%C3%B1o/'.encode('utf-8')
     actual = quote(u'/El Niño/'.encode('utf-8'))
-    assert(expected == actual)
+    assert expected == actual
 
 def test_unquote_bytes():
     expected = u'/El Niño/'.encode('utf-8')
     actual = unquote(u'/El%20Ni%C3%B1o/'.encode('utf-8'))
-    assert(expected == actual)
+    assert expected == actual
 
 def test_quote_string():
     expected = u'/El%20Ni%C3%B1o/'.encode('utf-8')
     actual = quote(u'/El Niño/')
-    assert(expected == actual)
+    assert expected == actual
 
 def test_unquote_string():
     expected = u'/El Niño/'.encode('utf-8')
     actual = unquote(u'/El%20Ni%C3%B1o/')
-    assert(expected == actual)
+    assert expected == actual
