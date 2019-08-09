6fa7a7aeb61fdb0b6c1c86e16fe6225a776a0a86
Markus Klein <markus.klein@blue-yonder.com>
2019-08-09 12:39:56 +0200

    use top level Cargo.toml for better IDE support

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ def build_native(spec):
     # build rust library
     build = spec.add_external_build(
         cmd=['cargo', 'build', '--release'],
-        path='./rust'
+        path='.'
     )
 
     spec.add_cffi_module(
         module_path='urlquote._native',
         dylib=lambda: build.find_dylib('urlquote', in_path='target/release'),
-        header_filename=lambda: build.find_header('native.h', in_path='.'),
+        header_filename=lambda: build.find_header('native.h', in_path='./rust'),
         rtld_flags=['NOW', 'NODELETE']
     )
 
