9007a4cc354b1311ad9a7aa50b74c0c828f41dbf
Markus Klein <markus.klein@blue-yonder.com>
2019-09-25 15:36:26 +0200

    make buffer threadlocal

--- urlquote/__init__.py
+++ urlquote/__init__.py
@@ -10,44 +10,50 @@ threadlocal = threading.local()
 # reallocated automatically by `_native_quote` and `_native_unquote` if its size should not be
 # large enough. It is ok to reset this buffer to a smaller value but it always needs to be a valid
 # buffer.
-threadlocal.buffer = ffi.new('uint8_t[]', 1)
+# To prevent race conditions on the buffer between threads, the buffer is made threadlocal
+threadlocal.buffer = ffi.new("uint8_t[]", 1)
+
 
 def _native_quote(value, quoting):
     """
     Urlencodes the given bytes
     """
-    buffer_len = len(threadlocal.buffer)
-    quoted_len = lib.quote(value, len(value), threadlocal.buffer, buffer_len, quoting)
+    buffer = threadlocal.buffer
+    buffer_len = len(buffer)
+    quoted_len = lib.quote(value, len(value), buffer, buffer_len, quoting)
     if quoted_len > buffer_len:
         # Our buffer has not been big enough to hold the quoted url. Let's allocate a buffer large
         # enough and try again.
-        threadlocal.buffer = ffi.new('uint8_t[]', quoted_len)
-        lib.quote(value, len(value), threadlocal.buffer, quoted_len, quoting)
+        buffer = ffi.new("uint8_t[]", quoted_len)
+        lib.quote(value, len(value), buffer, quoted_len, quoting)
 
-    return ffi.string(threadlocal.buffer, quoted_len)
+    return ffi.string(buffer, quoted_len)
+
 
 def _native_unquote(value):
     """
     Urldecodes the given bytes
     """
-    buffer_len = len(threadlocal.buffer)
-    unquoted_len = lib.unquote(value, len(value), threadlocal.buffer, buffer_len)
+    buffer = threadlocal.buffer
+    buffer_len = len(buffer)
+    unquoted_len = lib.unquote(value, len(value), buffer, buffer_len)
     if unquoted_len > buffer_len:
         # Our buffer has not been big enough to hold the unquoted url. Let's allocate a buffer large
         # enough and try again.
-        threadlocal.buffer = ffi.new('uint8_t[]', unquoted_len)
-        lib.unquote(value, len(value), threadlocal.buffer, unquoted_len)
+        buffer = ffi.new("uint8_t[]", unquoted_len)
+        lib.unquote(value, len(value), buffer, unquoted_len)
 
-    return ffi.string(threadlocal.buffer, unquoted_len)
+    return ffi.string(buffer, unquoted_len)
 
-def quote(value, quoting = DEFAULT_QUOTING):
+
+def quote(value, quoting=DEFAULT_QUOTING):
     """
     Performs string encoding and urlencodes the given string. Always returns utf-8 encoded bytes.
     """
     if not isinstance(value, six.binary_type):
         if not isinstance(value, six.text_type):
             value = str(value)
-        value = value.encode('utf-8')
+        value = value.encode("utf-8")
 
     return _native_quote(value, quoting)
 
@@ -59,6 +65,6 @@ def unquote(value):
     if not isinstance(value, six.binary_type):
         if not isinstance(value, six.text_type):
             value = str(value)
-        value = value.encode('utf-8')
+        value = value.encode("utf-8")
 
     return _native_unquote(value)
